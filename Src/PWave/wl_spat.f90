60d035354a1c14b87b5129fb8a9c9d5e1f0cb82e
Zhongwei Zhang <zhongweizhang123@gmail.com>
2024-12-10 16:47:11 +0800

    Add files via upload
    revise the 2D and 3D wavelet transform for the spatial phonon coherence calculations

--- Src/PWave/wl_spat.f90
+++ Src/PWave/wl_spat.f90
@@ -88,20 +88,20 @@ endif
 end subroutine Cal_spatial_1D
 
 
-   attributes(global)subroutine Cal_spatial_2D(modal_velocity,cell_position,cell_period,width_coh,&
-        vector_target,do_id,wavelet_results_node)
-        
+attributes(global)subroutine Cal_spatial_2D(modal_velocity,cell_position,cell_period,width_coh,&
+     vector_target,do_id,wavelet_results_node)
+     
 implicit none
 
 double precision,intent(in):: modal_velocity(:),cell_position(:,:),cell_period(:,:)
 double precision,intent(in):: width_coh(:),vector_target(:)
 integer,intent(in):: do_id(:)
 
-integer:: displace0,displace,k1,width_number
-integer:: j,kk,ia,ib,ic,k,Ln
+integer:: displace0,displace,width_number
+integer:: j,ia,Ln
 double precision:: width,temp1,temp3,temp4,x1(3),x2(3),a1,a2,b1,b2
-double precision,parameter::Size_WP=2.0
-double complex:: tmp,tmp1,tmp2,tmp3
+double precision,parameter:: Size_WP=2.0
+double complex:: tmp1,tmp2,tmp3
 double precision,intent(out):: wavelet_results_node(:,:)
 
 displace0=blockDim%x*(blockIdx%x-1)+threadIdx%x   ! position, time
@@ -109,7 +109,7 @@ displace=do_id(displace0)
 
 width_number=size(width_coh)
 width=maxval(width_coh)
-
+  
 x1(:)=cell_position(displace,:)
 
 a1=maxval((/cell_position(displace,1)-1.1*Size_WP*width,0.0d0/))
@@ -122,44 +122,44 @@ Ln=size(cell_position(:,1))
 
 if((a2-a1)>2.0*Size_WP*width .and. (b2-b1)>2.0*Size_WP*width) then
 
-    do j=1,width_number
-         
-        width=width_coh(j)
+  do j=1,width_number
 
-        temp1=0.0;tmp3=cmplx(0.0,0.0)
-        
-        do ia=1,Ln
-             
-            temp3=vector_target(1)*(cell_period(ia,1)-cell_period(displace,1))+&
+      width=width_coh(j)
+
+      temp1=0.0;tmp3=cmplx(0.0,0.0)
+      
+      do ia=1,Ln
+           
+          temp3=vector_target(1)*(cell_period(ia,1)-cell_period(displace,1))+&
                 vector_target(2)*(cell_period(ia,2)-cell_period(displace,2))+&
                 vector_target(3)*(cell_period(ia,3)-cell_period(displace,3))
-                            
-            x2(:)=cell_position(ia,:)
+                          
+          x2(:)=cell_position(ia,:)
 
-            temp4=(x2(1)-x1(1))**2+(x2(2)-x1(2))**2
-                            
-            if(sqrt(temp4)<Size_WP*width) then
+          temp4=(x2(1)-x1(1))**2+(x2(2)-x1(2))**2
+                          
+          if(sqrt(temp4)<Size_WP*width) then
 
-                    tmp1=exp(cmplx(0.0,2.0*3.1415926*temp3))*exp(-temp4/(width)**2*4.0*log(2.0))
-                    tmp2=tmp1*modal_velocity(ia)
-                        
-                    temp1=temp1+real(tmp1)**2+imag(tmp1)**2
-                    tmp3=tmp3+tmp2
-                        
-            endif
-	    
-         enddo
-        
-        temp1=sqrt(temp1)
-        
-        wavelet_results_node(j,displace0)=real(tmp3/temp1)**2+imag(tmp3/temp1)**2
-        wavelet_results_node(j,displace0)=wavelet_results_node(j,displace0)/sqrt(width) 
-               
-    enddo
-         
+                  tmp1=exp(cmplx(0.0,2.0*3.1415926*temp3))*exp(-temp4/(width)**2*4.0*log(2.0))
+                  tmp2=tmp1*modal_velocity(ia)
+                      
+                  temp1=temp1+real(tmp1)**2+imag(tmp1)**2
+                  tmp3=tmp3+tmp2
+                      
+          endif
+      
+       enddo
+      
+      temp1=sqrt(temp1)
+      
+      wavelet_results_node(j,displace0)=real(tmp3/temp1)**2+imag(tmp3/temp1)**2
+      wavelet_results_node(j,displace0)=wavelet_results_node(j,displace0)/sqrt(width)
+             
+  enddo
+  
 else
 
-    wavelet_results_node(:,displace)=0.0d0
+     wavelet_results_node(:,displace0)=0.0d0
 
 endif
 
@@ -175,11 +175,11 @@ double precision,intent(in):: modal_velocity(:),cell_position(:,:),cell_period(:
 double precision,intent(in):: width_coh(:),vector_target(:)
 integer,intent(in):: do_id(:)
 
-integer:: displace0,displace,k1,width_number
-integer:: j,kk,ia,ib,ic,k,Ln
+integer:: displace0,displace,width_number
+integer:: j,ia,Ln
 double precision:: width,temp1,temp3,temp4,x1(3),x2(3),a1,a2,b1,b2,c1,c2
-double precision,parameter::Size_WP=2.0
-double complex:: tmp,tmp1,tmp2,tmp3
+double precision,parameter:: Size_WP=2.0
+double complex:: tmp1,tmp2,tmp3
 double precision,intent(out):: wavelet_results_node(:,:)
 
 displace0=blockDim%x*(blockIdx%x-1)+threadIdx%x   ! position, time
@@ -187,7 +187,7 @@ displace=do_id(displace0)
 
 width_number=size(width_coh)
 width=maxval(width_coh)
-
+  
 x1(:)=cell_position(displace,:)
 
 a1=maxval((/cell_position(displace,1)-1.1*Size_WP*width,0.0d0/))
@@ -196,51 +196,51 @@ a2=minval((/cell_position(displace,1)+1.1*Size_WP*width,maxval(cell_position(:,1
 b1=maxval((/cell_position(displace,2)-1.1*Size_WP*width,0.0d0/))
 b2=minval((/cell_position(displace,2)+1.1*Size_WP*width,maxval(cell_position(:,2))/))
 
-c1=maxval((/cell_position(displace,3)-1.1*Size_WP*width,0.0d0/))
-c2=minval((/cell_position(displace,3)+1.1*Size_WP*width,maxval(cell_position(:,3))/))
+c1=maxval((/cell_position(displace,2)-1.1*Size_WP*width,0.0d0/))
+c2=minval((/cell_position(displace,2)+1.1*Size_WP*width,maxval(cell_position(:,2))/))
 
 Ln=size(cell_position(:,1))
 
 if((a2-a1)>2.0*Size_WP*width .and. (b2-b1)>2.0*Size_WP*width .and. (c2-c1)>2.0*Size_WP*width) then
 
- do j=1,width_number
+  do j=1,width_number
+
+      width=width_coh(j)
+
+      temp1=0.0;tmp3=cmplx(0.0,0.0)
       
-     width=width_coh(j)
+      do ia=1,Ln
+           
+          temp3=vector_target(1)*(cell_period(ia,1)-cell_period(displace,1))+&
+                vector_target(2)*(cell_period(ia,2)-cell_period(displace,2))+&
+                vector_target(3)*(cell_period(ia,3)-cell_period(displace,3))
+                          
+          x2(:)=cell_position(ia,:)
 
-     temp1=0.0;tmp3=cmplx(0.0,0.0)
-     
-     do ia=1,Ln
-          
-         temp3=vector_target(1)*(cell_period(ia,1)-cell_period(displace,1))+&
-             vector_target(2)*(cell_period(ia,2)-cell_period(displace,2))+&
-             vector_target(3)*(cell_period(ia,3)-cell_period(displace,3))
-                         
-         x2(:)=cell_position(ia,:)
+          temp4=(x2(1)-x1(1))**2+(x2(2)-x1(2))**2+(x2(3)-x1(3))**2
+                          
+          if(sqrt(temp4)<Size_WP*width) then
 
-         temp4=(x2(1)-x1(1))**2+(x2(2)-x1(2))**2+(x2(3)-x1(3))**2
-                         
-         if(sqrt(temp4)<Size_WP*width) then
-
-                 tmp1=exp(cmplx(0.0,2.0*3.1415926*temp3))*exp(-temp4/(width)**2*4.0*log(2.0))
-                 tmp2=tmp1*modal_velocity(ia)
-                     
-                 temp1=temp1+real(tmp1)**2+imag(tmp1)**2
-                 tmp3=tmp3+tmp2
-                     
-         endif
-     
-      enddo
-     
+                  tmp1=exp(cmplx(0.0,2.0*3.1415926*temp3))*exp(-temp4/(width)**2*4.0*log(2.0))
+                  tmp2=tmp1*modal_velocity(ia)
+                      
+                  temp1=temp1+real(tmp1)**2+imag(tmp1)**2
+                  tmp3=tmp3+tmp2
+                      
+          endif
+      
+       enddo
+      
       temp1=sqrt(temp1)
-
+      
       wavelet_results_node(j,displace0)=real(tmp3/temp1)**2+imag(tmp3/temp1)**2
       wavelet_results_node(j,displace0)=wavelet_results_node(j,displace0)/sqrt(width)
-                    
-    enddo
-      
+             
+  enddo
+  
 else
 
-    wavelet_results_node(:,displace)=0.0d0
+     wavelet_results_node(:,displace0)=0.0d0
 
 endif
 
